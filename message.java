fc5698cd8647676a264b6d0f325f727e80336d78
mannabathini <mannabathini@gmail.com>
2020-03-26 12:42:55 -0500

    Update message.java

--- message.java
+++ message.java
@@ -2,3 +2,18 @@
 2. Will be able to provide opportunity after completion of certificate in few months as presently
 our application are on linux servers and we might move to cloud applications. 
 3. Anusha has working experience in deploying application on AWS(EC2 instance, S3 bucket) using AIM script from Jules/ARM JOB
+
+
+
+Min education level ? Bachelors degree in relevant fields
+major fields of study required- mathematics, computer science or relevant fields
+Is experience required for this position ? Yes
+If yes, pls indicate the min number of months experience required to perform the position ? Masters+2, Bachelors+4
+Is there an alternate combination of education and experience that is accepted for this position ? Yes 
+If yes pls describe ?  Masters+2, Bachelors+4
+Is exp in an alternate/related job acceptable ? Yes
+If yes, please list app related jobs ? Programmer analyst, software engineer, software analyst
+Detailed job description ? Job position on top of it
+Fluency in foreign language ? No 
+Specific Skills or other requirements? C, C++, SQL, JAVA, J2EE
+

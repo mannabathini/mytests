e02df199146a64d8f2fd43fe9e2d889514766c2d
mannabathini <mannabathini@gmail.com>
2020-03-25 22:23:35 -0500

    Create message.java

---
+++ message.java
@@ -0,0 +1,4 @@
+1. Attending cohort meetings, attending events.
+2. Will be able to provide opportunity after completion of certificate in few months as presently
+our application are on linux servers and we might move to cloud applications. 
+3. Anusha has working experience in deploying application on AWS(EC2 instance, S3 bucket) using AIM script from Jules/ARM JOB
